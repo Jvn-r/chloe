c741d041e03e68d9c366f8b5309dfc2ba46baee6
Jeevan <rjeevan1495@gmail.com>
2026-01-04 12:01:04 +0530

    Refined, removed unnecessary returns, added exit, rearragned tokenizer

--- Chloe.c
+++ Chloe.c
@@ -12,7 +12,7 @@ int create_proc(char cmd_inp[]){
     //char cmd_inp[] = "notepad.exe";
 
     if (CreateProcessA(NULL,cmd_inp,NULL,NULL,FALSE,0,NULL,NULL,&si,&pi)){
-        printf("oka it did it");
+        //printf("oka it did it");
         WaitForSingleObject(pi.hProcess,INFINITE);
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
@@ -23,59 +23,57 @@ int create_proc(char cmd_inp[]){
     return 0;
 }
 
-char *reading(char *buffer,int size){
+void reading(char *buffer,int size){
     printf("\n$>");
     fgets(buffer,size,stdin);
-    printf("BUFFER FILLED\n");
-    return buffer;
+    //printf("BUFFER FILLED\n");
+    //return buffer;
 }
 
-char *remove_new_line(char *buffer){
+void remove_new_line(char *buffer){
     buffer[strcspn(buffer,"\n")] = '\0';
-    return buffer;
+    //return buffer;
 }
 
-char *tokenizer(char *buffer, char *argv[]){    
-    int i = 1;
+void tokenizer(char *buffer, char *argv[]){    
+    int i = 0;
     char *tok;
+    //printf("TOK BUFFER BEFOGRE TOK : %s\n",buffer);
     tok = strtok(buffer, " ");
-    if (tok == NULL){
-        return NULL;
-    }
-    argv[0] = tok;
-    printf("\n tok of 1 is don \n");
+    //if (tok == NULL)
+        //exit;
+    //argv[0] = tok;
+    //printf("\n tok of 1 is don \n");
 
     while(tok != NULL){
         argv[i] = tok;
         tok = strtok(NULL," ");
         printf("iteration %d \n",i);
         i++;
+        if(i == 49)
+            break;
     }
     argv[i] = NULL;
-    printf("Null added to end\n");
-    return NULL;
+    //printf("Null added to end\n");
+    //printf("TOK BUFFEEr %s\n",buffer);
 }
 
 int main(void){
     char gonk[100];
     char *argv[50];
-    /*reading(gonk,100);
-    printf("GONK REciveded\n");
-    printf("%s",gonk);
-    remove_new_line(gonk);    
-    tokenizer(gonk,argv);
-    printf("ARGV RETURNED\n");
-    int j = 0;
-    while(argv[j] != NULL){   
-        printf("args = %s\n",argv[j]);
-        j++;
-    }
-    create_proc(gonk);
-    */
+    char raw_cmd[100];
     while(1){
         reading(gonk,100);
+        //printf("reading %s\n",raw_cmd);
         remove_new_line(gonk);
+        strcpy(raw_cmd,gonk);
+        //printf("new line %s\n",gonk);
         tokenizer(gonk,argv);
-        create_proc(gonk);
+        //printf("TOK %s\n",gonk);
+        if(argv[0] == NULL)
+            continue;
+        if(strcmp(argv[0],"exit") == 0)
+            break;
+        create_proc(raw_cmd);
     }
 }
