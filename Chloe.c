f66745105072007321ecad49a63a84d5551a815a
Jeevan <rjeevan1495@gmail.com>
2026-01-10 22:17:26 +0530

    cd will cd now, intended in builts are done for now

--- Chloe.c
+++ Chloe.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 #include<windows.h>
 #include<string.h>
+#include <stdlib.h>
 
 int create_proc(char cmd_inp[]){
     STARTUPINFO si;
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si);
-    
     PROCESS_INFORMATION pi;
     ZeroMemory(&pi, sizeof(pi));
-    //char cmd_inp[] = "notepad.exe";
 
     if (CreateProcessA(NULL,cmd_inp,NULL,NULL,FALSE,0,NULL,NULL,&si,&pi)){
-        //printf("oka it did it");
         WaitForSingleObject(pi.hProcess,INFINITE);
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
@@ -26,36 +24,25 @@ int create_proc(char cmd_inp[]){
 void reading(char *buffer,int size){
     printf("\n$>");
     fgets(buffer,size,stdin);
-    //printf("BUFFER FILLED\n");
-    //return buffer;
 }
 
 void remove_new_line(char *buffer){
     buffer[strcspn(buffer,"\n")] = '\0';
-    //return buffer;
 }
 
 void tokenizer(char *buffer, char *argv[]){    
     int i = 0;
     char *tok;
-    //printf("TOK BUFFER BEFOGRE TOK : %s\n",buffer);
     tok = strtok(buffer, " ");
-    //if (tok == NULL)
-        //exit;
-    //argv[0] = tok;
-    //printf("\n tok of 1 is don \n");
 
     while(tok != NULL){
         argv[i] = tok;
         tok = strtok(NULL," ");
-        printf("iteration %d \n",i);
         i++;
         if(i == 49)
             break;
     }
     argv[i] = NULL;
-    //printf("Null added to end\n");
-    //printf("TOK BUFFEEr %s\n",buffer);
 }
 
 int inb_echo(char *argv[]) {
@@ -72,7 +59,18 @@ int inb_echo(char *argv[]) {
 }
 
 int inb_cd(char *argv[]) {
-    return 1;
+    BOOL x = FALSE;
+    if (argv[1]==NULL)
+        return inb_pwd(argv);
+    else
+        x = SetCurrentDirectory(argv[1]);
+    
+    if (x)
+        return 1;
+    else{
+        printf("error\n");
+        return 1;
+    }
 }
 
 int inb_pwd(char *argv[]) {
@@ -117,15 +115,12 @@ int main(void){
     char raw_cmd[100];
     while(1){
         reading(gonk,100);
-        //printf("reading %s\n",raw_cmd);
         remove_new_line(gonk);
         strcpy(raw_cmd,gonk);
-        //printf("new line %s\n",gonk);
         tokenizer(gonk,argv);
-        //printf("TOK %s\n",gonk);
+;
         if(argv[0] == NULL)
             continue;
-
         int inb_stat = in_built_check(argv);
         if(inb_stat == -1)
             break;
